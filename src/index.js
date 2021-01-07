515f12f31bb6d08ab480ecdaf1ac5f10d7d77b94
kristoferdowe <kristoferdowe@gmail.com>
2021-01-06 19:54:44 -0500

    hashrouter switched from browserrouter

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import './index.css';
 import App from './App';
 import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
   <App/>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
